4a8890f3b89957a562f4653426c867e3245ea4d2
Alina <maximovaalina5@gmail.com>
2023-08-04 12:01:17 +0300

    15 спринт

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,6 +1,27 @@
 const jwt = require('jsonwebtoken');
 const AuthError = require('../utils/AuthError');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+const auth = (req, res, next) => {
+  const { authorization } = req.headers;
+  const bearer = 'Bearer ';
+  if (!authorization || !authorization.startsWith(bearer)) {
+    return next(new AuthError('Неверный логин или пароль'));
+  }
+  const token = authorization.replace(bearer, '');
+  let payload;
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    req.user = payload;
+  } catch (err) {
+    return next(new AuthError('Неверный логин или пароль'));
+  }
+
+  return next();
+};
+
+/*
 const auth = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
@@ -16,5 +37,5 @@ const auth = (req, res, next) => {
 module.exports = {
   auth,
 };
-
+*/
 module.exports = { auth };
