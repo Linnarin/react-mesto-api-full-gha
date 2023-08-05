885da50e8d6bdf57ae8d5175d66c984c10a0521e
Alina <maximovaalina5@gmail.com>
2023-08-05 19:42:41 +0300

    15 спринт

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -51,28 +51,6 @@ const deleteCard = (req, res, next) => {
 };
 
 const likeCard = (req, res, next) => {
-  const { id } = req.params;
-  const idUser = req.user._id;
-  Card.findByIdAndUpdate(id, { $addToSet: { likes: [idUser] } }, { new: true })
-    .orFail(() => next(new NotFound('Карточка не найдена')))
-    .then((card) => {
-      if (!card) {
-        next(new NotFound('Карточка с указанным id не существует'));
-      } else {
-        res.send(card);
-      }
-    })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        next(new BadRequest('Введены некоректные данные'));
-      } else {
-        next(err);
-      }
-    });
-};
-
-/*
-const likeCard = (req, res, next) => {
   const { cardId } = req.params;
 
   Card.findByIdAndUpdate(
@@ -96,26 +74,7 @@ const likeCard = (req, res, next) => {
       }
     });
 };
-*/
 
-const deleteLikeCard = (req, res, next) => {
-  const { id } = req.params;
-  const idUser = req.user._id;
-  Card.findByIdAndUpdate(id, { $pull: { likes: idUser } }, { new: true })
-    .orFail(() => next(new NotFound('Карточка не найдена')))
-    .then((card) => {
-      res.send(card);
-    })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        next(new NotFound('Переданы некорректные данные для удаления лайка'));
-      } else {
-        next(err);
-      }
-    });
-};
-
-/*
 const deleteLikeCard = (req, res, next) => {
   const { cardId } = req.params;
   Card.findByIdAndUpdate(
@@ -137,7 +96,7 @@ const deleteLikeCard = (req, res, next) => {
       }
     });
 };
-*/
+
 module.exports = {
   createCard,
   getCards,
